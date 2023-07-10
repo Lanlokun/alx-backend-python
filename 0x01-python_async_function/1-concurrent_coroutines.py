c8c82cb0646f65f24e43a9cf12e56b7805346b69
malik <mlanlokun@gmail.com>
2023-07-11 00:46:50 +0300

    using heapq

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,23 @@
 
 """ execute multiple coroutines at the same time with async """
 
-import asyncio
+import asyncio, heapq
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
-    """ return the list of all the delays in ascending order """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    delays = []
+
+    async def wait_and_append_delay():
+        delay = await wait_random(max_delay)
+        heapq.heappush(delays, delay)
+
+    tasks = [wait_and_append_delay() for _ in range(n)]
+    await asyncio.gather(*tasks)
+
+    sorted_delays = []
+    while delays:
+        sorted_delays.append(heapq.heappop(delays))
+
+    return sorted_delays
