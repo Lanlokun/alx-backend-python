3a4a2c8220809a524f0383601f39003d5962f23e
malik <mlanlokun@gmail.com>
2024-12-30 16:57:27 +0800

    serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework import serializers
-from .models import User, Message, Conversation
 from rest_framework.exceptions import ValidationError
+from .models import User, Conversation, Message
+import uuid
 
-
+# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
-    
+
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}" if obj.first_name and obj.last_name else obj.username
     
     class Meta:
         model = User
-        fields = ['user_id', 'username', 'first_name', 'last_name', 'email', 'phone_number']
-
+        fields = ['user_id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'profile_picture', 'status', 'full_name']
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer()  # Nested serializer for the sender (User)
-    sent_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")  # Format the sent_at field
+    sender = UserSerializer() 
+    sent_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     message_body = serializers.CharField(max_length=2000)
     
     def validate_message_body(self, value):
@@ -30,16 +30,16 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at', 'created_at']
-
+        
 class ConversationSerializer(serializers.ModelSerializer):
-    users = UserSerializer(many=True)  # Many-to-many relationship, serialize users
-    messages = MessageSerializer(many=True)  # Many-to-one relationship, serialize messages within the conversation
-
+    users = UserSerializer(many=True)
+    messages = MessageSerializer(many=True)
+    
     total_messages = serializers.SerializerMethodField()
-
+    
     def get_total_messages(self, obj):
-            return obj.messages.count()
-
+        return obj.messages.count()
+    
     def validate_users(self, value):
         """
         Ensure a conversation has at least two users.
@@ -47,6 +47,7 @@ class ConversationSerializer(serializers.ModelSerializer):
         if len(value) < 2:
             raise ValidationError("A conversation must involve at least two users.")
         return value
+
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+        fields = ['conversation_id', 'users', 'messages', 'created_at', 'total_messages']
