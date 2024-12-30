da8c4808bae5648231763232a40c32b75c9e1a01
malik <mlanlokun@gmail.com>
2024-12-30 15:34:54 +0800

    project set up

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework.permissions.IsAuthenticated",
+    "DEFAULT_AUTHENTICATION_CLASSES", 
+    "rest_framework.authentication.SessionAuthentication"
     'messaging_app',
 ]
 
